d0e8177fb2905c3eec6cfd6da7f8d7a7fedfb524
Crispybanks // inkbliss <112598685+Crispybanks@users.noreply.github.com>
2024-03-17 23:17:43 -0400

    Help command rps_game.cpp
    Added a help command for more help on RPS

--- rps_game.cpp
+++ rps_game.cpp
@@ -5,22 +5,32 @@
 
 int main() {
     const std::string RPS_LIST[3] = {"Rock", "Paper", "Scissors"};
-	const std::string RPS_LIST_KEYWORDS[3] = {"R", "P", "S"}; // R - Rock, P - Paper, S - Scissors
-
+	const std::string RPS_LIST_KEYWORDS[3] = {"R", "P", "S"}; // Keywords for Rock, Paper, Scissors 
+	
+	// Seed the random number generator using the current time to ensure different outcomes each time the game is played
     std::srand(static_cast<unsigned int>(std::time(nullptr)));
+	// Calculate the size of the RPS_LIST array to determine the number of elements (Rock, Paper, Scissors)
+	int rps_list_size = sizeof(RPS_LIST) / sizeof(RPS_LIST[0]);
 
-    int rps_list_size = sizeof(RPS_LIST) / sizeof(RPS_LIST[0]);
+	std::string rps_help_command = "?help";
 
-    std::cout << "\nWelcome to Rock Paper Scissors Shoot!\n\n" << std::endl;
+    std::cout << "\nWelcome to Rock Paper Scissors Shoot! Need Help? Type: '?help' for a guide\n\n" << std::endl;
 
     while (true) {
         int random_rps = std::rand() % rps_list_size;
 
         std::cout << "Enter Rock/Paper/Scissors (R, P, S): ";
-        std::string check_for_valid_input;
+        std::string check_for_valid_input; // Variable check if user entered a valid input option
         std::getline(std::cin, check_for_valid_input);
 
-        if (check_for_valid_input == "R" || check_for_valid_input == "Rock" || check_for_valid_input == "rock" || check_for_valid_input == "r") { // Rock
+		if (check_for_valid_input == rps_help_command) {
+			std::cout << "\n\nWelcome to the help menu!\n" << std::endl;
+			std::cout << "- To play simply enter: Rock, Paper or Scissors." << '\n';
+		
+			std::cout << "- Instead of typing such long characters you could use keywords like: R, r, P, p, S, s." << std::endl;
+			std::cout << "- All of those are valid you can also type them like: Rock, rock, Paper, paper, Scissors, scissors." << std::endl;
+			std::cout << "- If you need additional help with anything other than keywords refer to the RPS official github link: \"https://github.com/Crispybanks/RPS-Game\"\n\n" << std::endl;	
+		} else if (check_for_valid_input == "R" || check_for_valid_input == "Rock" || check_for_valid_input == "rock" || check_for_valid_input == "r" || check_for_valid_input == rps_help_command) { // check if user entered a valid input option for the option 'Rock'
             std::cout << "\n- Opponent Entered: " << RPS_LIST[random_rps] << std::endl;
 
             if (random_rps == 0) {
@@ -32,7 +42,7 @@ int main() {
             } else {
                 std::cout << "- Error; so that means... Draw!\n" << std::endl;
             }
-        } else if (check_for_valid_input == "P" || check_for_valid_input == "Paper" || check_for_valid_input == "paper" || check_for_valid_input == "p") { // Paper
+        } else if (check_for_valid_input == "P" || check_for_valid_input == "Paper" || check_for_valid_input == "paper" || check_for_valid_input == "p" || check_for_valid_input == rps_help_command) { // check if user entered a valid input option for the option 'Paper'
             std::cout << "\n- Opponent Entered: " << RPS_LIST[random_rps] << std::endl;
 
             if (random_rps == 0) {
@@ -44,7 +54,7 @@ int main() {
             } else {
                 std::cout << "- Error; so that means... Draw!\n" << std::endl;
             }
-        } else if (check_for_valid_input == "S" || check_for_valid_input == "Scissors" || check_for_valid_input == "scissors" || check_for_valid_input == "s") { // Scissors
+        } else if (check_for_valid_input == "S" || check_for_valid_input == "Scissors" || check_for_valid_input == "scissors" || check_for_valid_input == "s") { // check if user entered a valid input option for the option 'Scissors'
             std::cout << "\n- Opponent Entered: " << RPS_LIST[random_rps] << std::endl;
 
             if (random_rps == 0) {
@@ -56,8 +66,8 @@ int main() {
             } else {
                 std::cout << "- Error; so that means... Draw!\n" << std::endl;
             }
-        } else {
-            std::cout << "\n- Choose between: Rock, Paper, Scissors; Keywords (R, P, S)\n" << std::endl;
+        } else { // output below if the user entered something other than the options
+            std::cout << "\n- Choose between: Rock, Paper, Scissors; Keywords (R, P, S) If you need more help do: '?help'\n" << std::endl;
         }
     }
 
